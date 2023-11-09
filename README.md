283039f967799df459ff9e9f32475e73a9a00ff4
PoopFace <danilsteam-88@hotmail.com>
2023-11-09 03:07:37 +0300

    i

---
+++ README.md
@@ -0,0 +1,36 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Live Site URL: [https://teal-sawine-28c0c5.netlify.app/](https://teal-sawine-28c0c5.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Preprocessor SCSS
+- JavaScript
+- React
+
+## Author
+
+- Frontend Mentor - [@StinkyCodeMACHINE](https://www.frontendmentor.io/profile/StinkyCodeMACHINE)
---
+++ src/App.vue
@@ -0,0 +1,139 @@
+<script setup>
+import { ref } from "vue";
+const headerOptions = ref([
+  {
+    name: "Features",
+    subOptions: [
+      {
+        name: "Todo List",
+        icon: "/assets/icon-todo.svg",
+      },
+      {
+        name: "Calendar",
+        icon: "/assets/icon-calendar.svg",
+      },
+      {
+        name: "Reminders",
+        icon: "/assets/icon-reminders.svg",
+      },
+      {
+        name: "Planning",
+        icon: "/assets/icon-planning.svg",
+      },
+    ],
+  },
+  {
+    name: "Company",
+    subOptions: [
+      {
+        name: "History",
+      },
+      {
+        name: "Our Team",
+      },
+      {
+        name: "Blog",
+      },
+    ],
+  },
+  {
+    name: "Careers",
+  },
+  {
+    name: "About",
+  },
+]);
+</script>
+
+<template>
+  <header class="header">
+    <img src="/assets/logo.svg" alt="snap logo" class="header__logo" />
+    <nav>
+      <ul class="header__menu">
+        <li
+          class="header__menu__option-container"
+          v-for="option in headerOptions"
+          @mouseover="
+            ($event) => {
+              $event.target
+                .closest('.header__menu__option-container')
+                .classList.add(`hovered-over`);
+            }
+          "
+          @mouseleave="
+            ($event) => {
+              $event.target
+                .closest('.header__menu__option-container')
+                .classList.remove(`hovered-over`);
+            }
+          "
+        >
+          <div class="header__menu__option-container__option">
+            <div class="header__menu__option-container__option__text">
+              {{ option.name }}
+            </div>
+            <img
+              src="/assets/icon-arrow-down.svg"
+              alt="arrow"
+              class="header__menu__option-container__option__arrow"
+              v-if="Object.keys(option).includes(`subOptions`)"
+            />
+          </div>
+          <div
+            class="header__menu__option-container__suboptions"
+            v-if="Object.keys(option).includes(`subOptions`)"
+          >
+            <div
+              class="header__menu__option-container__suboptions__suboption"
+              v-for="subOption in option.subOptions"
+            >
+              <img
+                :src="subOption.icon"
+                alt="todo icon"
+                class="header__menu__option-container__suboptions__suboption__icon"
+                v-if="Object.keys(subOption).includes(`icon`)"
+              />
+              <div
+                class="header__menu__option-container__suboptions__suboption__text"
+              >
+                {{ subOption.name }}
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="header__menu__login">
+          <button>Login</button>
+        </li>
+        <li class="header__menu__register">
+          <button>Register</button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main class="main">
+    <div class="main__info">
+      <h1>
+        Make <br />
+        remote work
+      </h1>
+      <p>
+        Get your team in sync, no matter your location. Streamline processes,
+        create team rituals, and watch productivity soar.
+      </p>
+      <button>Learn more</button>
+      <div class="main__info__logos">
+        <img src="/assets/client-databiz.svg" alt="databiz logo" />
+        <img src="/assets/client-audiophile.svg" alt="audiophile logo" />
+        <img src="/assets/client-meet.svg" alt="meet logo" />
+        <img src="/assets/client-maker.svg" alt="maker logo" />
+      </div>
+    </div>
+    <img
+      src="/assets/image-hero-desktop.png"
+      alt="some guy with a laptop"
+      class="main__giga-img"
+    />
+  </main>
+</template>
+
+<style scoped></style>
